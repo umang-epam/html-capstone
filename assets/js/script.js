102a297beaa90917f84a899583df5d08856c65a7
umang-epam <umang_saxena@epam.com>
2026-07-03 21:45:01 +0530

    Feat : Initial Setup

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+console.log("Project Loaded Successfully 🚀");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Tailwind is working! 🎉");
+});
